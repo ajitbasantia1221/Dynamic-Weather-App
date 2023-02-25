15e2632649332a6c8b20cdae27b04127b85ed91c
Ajit Kumar Basantia <ajitkumarbasantia@gmail.com>
2023-02-25 19:58:11 +0530

    First Commit

---
+++ WeatherAppNodeJs/index.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+var requests = require('requests');
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+const replaceval =(tempVal,orgVal) => {
+    let temperature =tempVal.replace("{%tempval%}",orgVal.main.temp);
+    temperature =temperature.replace("{%tempval%}",orgVal.main.temp_min);
+    temperature =temperature.replace("{%tempmin%}",orgVal.main.temp_max);
+    temperature =temperature.replace("{%tempmax%}",orgVal.main.temp);
+    temperature =temperature.replace("{%location%}",orgVal.name);
+    temperature =temperature.replace("{%country%}",orgVal.sys.country);
+    temperature =temperature.replace("{%tempstatus%}",orgVal.weather[0].main);
+    
+    return temperature;
+}
+
+const server = http.createServer((req, res) => {
+
+    if (req.url == "/") {
+        requests('https://api.openweathermap.org/data/2.5/weather?q=Bengaluru&appid=417e68940396f3a257004ffcb5ba1916')
+            .on('data',(chunk) =>{
+                const objdata = JSON.parse(chunk);
+                const arrData = [objdata];
+                //console.log(arrData[0].main.temp);
+                const realTimeData = arrData.map((val) => replaceval(homeFile,val)).join("");
+                res.write(realTimeData);
+                //console.log(realTimeData);
+            })
+            .on('end',(err) =>{
+                if (err) return console.log('connection closed due to errors', err);
+                res.end();
+            });
+    }
+});
+server.listen(8000,"127.0.0.1");
